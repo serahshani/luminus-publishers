10495a1ca0b479bd8d5f26cbd9f2bb405bdc115f
lewismunene020 <lewismunene020@gmail.com>
2024-10-07 17:08:06 +0300

    Fix: Position TopBar and Header correctly
    Moves the `TopBar` and `Header` components into a fixed container to ensure they remain at the top of the screen, regardless of scrolling. This resolves issues with overlapping content and improves the overall layout consistency.

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,8 +3,10 @@
 </script>
 <template>
   <div class="w-full h-full overflow-x-hidden">
-    <TopBar />
-    <Header />
+    <div class="fixed top-0 left-0 right-0 z-50">
+      <TopBar />
+      <Header />
+    </div>
     <slot />
     <Footer />
   </div>
